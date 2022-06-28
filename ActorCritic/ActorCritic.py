b8fc589eecec0a5139a6cc617d4a6d015be04f1f
JangsilHyun <wogus0948@naver.com>
2022-06-28 17:45:23 +0900

    fixed ActorCritic update_weight

--- ActorCritic/ActorCritic.py
+++ ActorCritic/ActorCritic.py
@@ -119,13 +119,14 @@ class ActorCritic():
 
             # get actor loss
             log_prob = torch.log(self.pi(s_t, a_t) + self.ups)
-            advantage = Variable(r_tt + self.value(s_tt) - self.value(s_t))
+            advantage = r_tt + self.value(s_tt) - self.value(s_t)
             actor_loss = -(advantage * log_prob)
 
             # get critic loss
             value = self.value(s_t)
             next_value = self.value(s_tt)
             critic_loss = 1/2 * (r_tt + self.discount_rate * next_value - value).pow(2)
+
             loss = actor_loss + critic_loss + 0.001 * entropy_term
 
             self.optimizer.zero_grad()
