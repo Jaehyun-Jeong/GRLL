2ad59aa68f36f5fa292b7c68af3bfed0a1feb0b4
JangsilHyun <wogus0948@naver.com>
2022-07-04 18:39:15 +0900

    made DQN tesfile for Acrobot, MoutainCar

---
+++ TRIALS/DQN_Acrobot.py
@@ -0,0 +1,53 @@
+import sys
+sys.path.append("../") # to import module
+
+# PyTorch
+import torch
+import torch.optim as optim
+
+# import model
+from DQN.models import ANN_V2
+from DQN.DQN import DQN
+
+# Environment 
+import gym
+
+MAX_EPISODES = 3000
+MAX_TIMESTEPS = 1000
+MAX_REPLAYMEMORY = 10000
+
+ALPHA = 0.0001 # learning rate
+GAMMA = 0.99 # discount rate
+epsilon = 0.3
+
+gym_name = 'Acrobot-v1'
+
+# device to use
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+# set environment
+env = gym.make(gym_name)
+
+# set ActorCritic
+num_actions = env.action_space.n
+num_states = env.observation_space.shape[0]
+model = ANN_V2(num_states, num_actions).to(device)
+optimizer = optim.Adam(model.parameters(), lr=ALPHA)
+
+params_dict = {
+    'device': device, # device to use, 'cuda' or 'cpu'
+    'env': env, # environment like gym
+    'model': model, # torch models for policy and value funciton
+    'optimizer': optimizer, # torch optimizer
+    'maxTimesteps': MAX_TIMESTEPS, # maximum timesteps agent take 
+    'discount_rate': GAMMA, # step-size for updating Q value
+    'epsilon': epsilon,
+    'maxMemory': MAX_REPLAYMEMORY,
+    'numBatch': 64
+}
+
+# Initialize Actor-Critic Mehtod
+DeepQN = DQN(**params_dict)
+
+# TRAIN Agent
+DeepQN.train(MAX_EPISODES, testPer=1, useTensorboard=True, tensorboardTag="DQN_AL"+str(ALPHA)+"_GA"+str(GAMMA)+"_"+gym_name)
