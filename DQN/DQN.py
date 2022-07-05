11752f506b2a421341fc2e03dfc6e8b09bb35d2c
JangsilHyun <wogus0948@naver.com>
2022-07-05 09:00:43 +0900

    fixed DQN loss

--- DQN/DQN.py
+++ DQN/DQN.py
@@ -114,7 +114,7 @@ class DQN():
             r_tt = Transition.reward
             
             target = Variable(r_tt + self.discount_rate * self.max_value(s_tt) * (not done))
-            loss += 1/2 * (target - self.pi(s_t, a_t)).pow(2)
+            loss += target - self.pi(s_t, a_t)
 
         loss = loss/lenLoss
         
