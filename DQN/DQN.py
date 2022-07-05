a69ff169bea5af3dd8dbfe9b559a1b78136f13a7
JangsilHyun <wogus0948@naver.com>
2022-07-05 17:36:45 +0900

    final

--- DQN/DQN.py
+++ DQN/DQN.py
@@ -275,3 +275,247 @@ if __name__ == "__main__":
     # TRAIN Agent
     DeepQN.train(MAX_EPISODES, isRender=False, useTensorboard=True, tensorboardTag="CartPole-v1")
 
+class ADQN():
+
+    '''
+    param_dict = {
+        'device': device to use, 'cuda' or 'cpu'
+        'env':  environment like gym
+        'model': torch models for policy and value funciton
+        'optimizer': torch optimizer
+        'maxTimesteps': maximum timesteps agent take 
+        'discount_rate': step-size for updating Q value
+        'maxMemory': capacitiy of Replay Memory
+        'numBatch': number of batches
+        'eps': { # for epsilon scheduling
+            'start': 0.9,
+            'end': 0.05,
+            'decay': 200
+        }
+    }
+    '''
+
+    def __init__(self, **params_dict):
+        super(ADQN, self).__init__()
+
+        # init parameters 
+        self.device = params_dict['device']
+        self.env = params_dict['env']
+        self.model = params_dict['model']
+        self.optimizer = params_dict['optimizer']
+        self.maxTimesteps = params_dict['maxTimesteps'] 
+        self.discount_rate = params_dict['discount_rate']
+        self.replayMemory = ReplayMemory(params_dict['maxMemory'])
+        self.numBatch = params_dict['numBatch']
+        self.eps = params_dict['eps']
+        self.steps_done = 0 # eps scheduling
+
+        # torch.log makes nan(not a number) error, so we have to add some small number in log function
+        self.ups=1e-7
+
+    # In Reinforcement learning, pi means the function from state space to action probability distribution
+    # Returns probability of taken action a from state s
+    def pi(self, s, a):
+        s = torch.Tensor(s).to(self.device)
+        value = self.model.forward(s)
+        value = torch.squeeze(value, 0)
+        return value[a]
+
+    def get_eps(self):
+        import math
+
+        eps_start = self.eps['start']
+        eps_end = self.eps['end']
+        eps_decay = self.eps['decay']
+
+        eps_threshold = eps_end + (eps_start - eps_end) * math.exp(-1. * self.steps_done / eps_decay)
+
+        return eps_threshold
+    
+    # Returns the action from state s by using multinomial distribution
+    def get_action(self, s): # epsilon 0 for greedy action
+        with torch.no_grad():
+            s = torch.tensor(s).to(self.device)
+            values = self.model.forward(s)
+            probs = torch.squeeze(values, 0)
+
+            if random.random() >= self.get_eps():
+                action = torch.argmax(probs, dim=0)
+            else:
+                a = torch.rand(probs.shape).multinomial(num_samples=1)
+                a = a.data
+                action = a[0]
+
+            return action
+  
+    # Returns a value of the state (state value function in Reinforcement learning)
+    def max_value(self, s):
+        s = torch.tensor(s).to(self.device)
+        value = self.model.forward(s)
+        value = torch.squeeze(value, 0)
+        maxValue = torch.max(value)
+
+        return maxValue
+
+    # Update weights by using Actor Critic Method
+    def update_weight(self):
+        loss = 0
+        batch_size = self.numBatch if self.numBatch <= self.replayMemory.memory.__len__() else self.replayMemory.__len__() # if memory is smaller then numBatch, then just use all data
+        batches = self.replayMemory.sample(batch_size)
+        lenLoss = len(batches)
+
+        # update by using mini-batch Gradient Descent
+        for Transition in batches:
+            s_t = Transition.state
+            a_t = Transition.action
+            done = Transition.done
+            s_tt = Transition.next_state
+            r_tt = Transition.reward
+            
+            target = Variable(r_tt + self.discount_rate * self.max_value(s_tt) * (not done))
+            loss += 1/2 * (target - self.pi(s_t, a_t)).pow(2)
+
+        loss = loss/lenLoss
+        
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
+
+        self.steps_done += 1
+
+    def test(self, isRender=False):
+        state = self.env.reset()
+        done = False
+        rewards = []
+
+        for timesteps in range(self.maxTimesteps):
+            if isRender:
+                self.env.render()
+
+            action = self.get_action(state)
+            next_state, reward, done, _ = self.env.step(action.tolist())
+
+            rewards.append(reward)
+            state = next_state
+
+            if done or timesteps == self.maxTimesteps-1:
+                break
+
+        return sum(rewards)
+
+    def train(self, maxEpisodes, testPer=10, isRender=False, useTensorboard=False, tensorboardTag="DQN"):
+        try:
+            returns = []
+
+            #<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+            # TENSORBOARD
+            
+            if useTensorboard:
+                from torch.utils.tensorboard import SummaryWriter
+                self.writer = SummaryWriter()
+
+            #>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+            for i_episode in range(maxEpisodes):
+
+                state = self.env.reset()
+                done = False
+                
+                #==========================================================================
+                # MAKE TRAIN DATA
+                #==========================================================================
+
+                # while not done:
+                for timesteps in range(self.maxTimesteps):
+
+                    if isRender:
+                       self.env.render()
+
+                    action = self.get_action(state)
+                    next_state, reward, done, _ = self.env.step(action.tolist())
+                    self.replayMemory.push(state, action, done, next_state, reward)
+                    state = next_state
+
+                    if done or timesteps == self.maxTimesteps-1:
+                        break
+
+                    # train
+                    self.update_weight()
+
+                #==========================================================================
+                # TEST
+                #==========================================================================
+                if (i_episode+1) % testPer == 0: 
+
+                    cumulative_rewards = self.test()
+                    returns.append(cumulative_rewards)
+
+                    #<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+                    # TENSORBOARD
+
+                    if useTensorboard:
+                        self.writer.add_scalars("Returns", {tensorboardTag: returns[-1]}, i_episode+1)
+
+                    #>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+                    print("Episode: {0:<10} return: {1:<10}".format(i_episode + 1, returns[-1]))
+
+        except KeyboardInterrupt:
+            print("==============================================")
+            print("KEYBOARD INTERRUPTION!!=======================")
+            print("==============================================")
+
+            plt.plot(range(len(returns)), returns)
+        finally:
+            plt.plot(range(len(returns)), returns)
+
+        self.env.close()
+
+if __name__ == "__main__":
+
+    from models import ANN_V1 # import model
+    import gym # Environment 
+
+    MAX_EPISODES = 10000
+    MAX_TIMESTEPS = 1000
+    MAX_REPLAYMEMORY = 10000
+
+    ALPHA = 0.1e-3 # learning rate
+    GAMMA = 0.99 # discount_rate
+
+    # device to use
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    # set environment
+    env = gym.make("CartPole-v0")
+    #env = gym.make("Acrobot-v1")
+    #env = gym.make("MountainCar-v0")
+
+    num_actions = env.action_space.n
+    num_states = env.observation_space.shape[0]
+
+    ACmodel = ANN_V1(num_states, num_actions).to(device)
+    optimizer = optim.Adam(ACmodel.parameters(), lr=ALPHA)
+
+    param_dict = {
+        'device': device, # device to use, 'cuda' or 'cpu'
+        'env': env, # environment like gym
+        'model': ACmodel, # torch models for policy and value funciton
+        'optimizer': optimizer, # torch optimizer
+        'maxTimesteps': MAX_TIMESTEPS, # maximum timesteps agent take 
+        'discount_rate': GAMMA, # step-size for updating Q value
+        'maxMemory': MAX_REPLAYMEMORY, # capacitiy of Replay Memory
+        'numBatch': 64, # number of batches
+        'eps': { # for epsilon scheduling
+            'start': 0.9,
+            'end': 0.05,
+            'decay': 200
+        }
+    }
+
+    # Initialize DQN Mehtod
+    DeepQN = DQN(**param_dict)
+
+    # TRAIN Agent
+    DeepQN.train(MAX_EPISODES, isRender=False, useTensorboard=True, tensorboardTag="CartPole-v1")
+
