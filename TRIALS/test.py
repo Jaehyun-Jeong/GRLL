afdbd1fc03b2a0095deac3edb7d2f682a3288c35
Jaehyun-Jeong <wogus0948@naver.com>
2022-07-27 06:06:22 +0000

    fixed

--- TRIALS/test.py
+++ TRIALS/test.py
@@ -1,16 +1,16 @@
 import pickle
+import torch
 
-file = open("./saved_models/DQN_RacingEnv_v0.obj", 'rb')
-dataPickle = file.read()
-file.close()
+fileOne = open("./saved_models/DQN_RacingEnv_v0.obj", 'rb')
+dataPickle = fileOne.read()
+fileOne.close()
 
 save_dict = pickle.loads(dataPickle)
-
-save_dict['modelStateDict'] = self.model.state_dict()
+save_dict['modelStateDict'] = save_dict.model.state_dict()
 save_dict.pop('model', None)
-save_dict['optimizerStateDict'] = self.model.state_dict()
+save_dict['optimizerStateDict'] = save_dict.optmizer.state_dict()
 save_dict.pop('optimizer', None)
 
-file = open(saveDir, 'wb')
-file.write(pickle.dumps(save_dict))
-file.close()
+fileTwo = open(saveDir, 'wb')
+fileTwo.write(pickle.dumps(save_dict))
+fileTwo.close()
