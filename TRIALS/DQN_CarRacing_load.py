cc36ac77fa40e8e611294a29621024f55286e7c8
JangsilHyun <wogus0948@naver.com>
2022-07-27 14:17:17 +0900

    train

---
+++ TRIALS/DQN_CarRacing_load.py
@@ -0,0 +1,51 @@
+import sys
+sys.path.append("../") # to import module
+
+# PyTorch
+import torch
+import torch.optim as optim
+
+# import model
+from module.ValueBased.models import ANN_V2
+from module.ValueBased import DQN
+
+# Environment 
+from module.envs.CarRacing import RacingEnv_v0
+
+MAX_EPISODES = 3000
+MAX_TIMESTEPS = 1000
+MAX_REPLAYMEMORY = 10000
+
+ALPHA = 0.0001 # learning rate
+GAMMA = 0.99 # discount rate
+
+# device to use
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+# set environment
+env = RacingEnv_v0()
+
+# set ActorCritic
+num_actions = env.num_actions
+num_states = env.num_obs
+model = ANN_V2(num_states, num_actions).to(device)
+optimizer = optim.Adam(model.parameters(), lr=ALPHA)
+
+params_dict = {
+    'device': device, # device to use, 'cuda' or 'cpu'
+    'env': env, # environment like gym
+    'model': model, # torch models for policy and value funciton
+    'optimizer': optimizer, # torch optimizer
+}
+
+# Initialize Actor-Critic Mehtod
+DeepQN = DQN(**params_dict)
+
+# load pretrained model
+DeepQN.load("./saved_models/DQN_RacingEnv_v0.obj")
+
+# TRAIN Agent
+DeepQN.train(MAX_EPISODES)
+
+# save model
+DeepQN.save("./saved_models/DQN_RacingEnv_v0.obj")
