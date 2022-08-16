e1665345062d3da342ebbda4f2a9ef3de151e6d9
JangsilHyun <wogus0948@naver.com>
2022-08-16 16:24:47 +0900

    added contents to 8월3주차 연구일지

--- ray-rllib/test.py
+++ ray-rllib/test.py
@@ -4,9 +4,8 @@ from ray.tune.logger import pretty_print
 
 ray.init()
 
-config = dqn.DEFAULT_CONFIG.copy()
-
 # === 저번주의 테스트와 동일하게 파라미터 설정 ===
+config = dqn.DEFAULT_CONFIG.copy()
 config["num_gpus"] = 0 # gpu는 사용하지 않는다.
 config["num_workers"] = 1 # 하나의 에이전트만 실행한다.
 config["lr"] = 0.001
@@ -27,11 +26,16 @@ config["hiddens"] = [64]
 config["framework"] = "torch" # 작성자의 모듈과 동일하게 PyTorch를 사용한다.
 #=================================================
 
-
 trainer = dqn.DQNTrainer(config=config, env="CartPole-v0")
+
+trainer.validate_env()
+raise ValueError("test")
 
 # 한 번의 반복에 1000번의 timestep이 진행된다. 따라서 100 * 1000 = 10만 번의 timestep이 진행된다.
 for i in range(100):
-    result = trainer.train()
-    print(pretty_print(result))
-    print("====================================================")
+    if i <= 99:
+        result = trainer.train()
+    else:
+        trainer.train() 
+
+print(pretty_print(result))
