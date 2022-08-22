1f7dc97fdeffb3f36ea9e1771721b826f1378ec6
Jaehyun-Jeong <wogus0948@naver.com>
2022-08-22 01:28:54 +0000

    test

--- TRIALS/envs/CarRacing-v3/DQN_load.py
+++ TRIALS/envs/CarRacing-v3/DQN_load.py
@@ -40,6 +40,6 @@ DeepQN = DQN(**params_dict)
 DeepQN.load("../../saved_models/CarRacing_v3/DQN_lr1e-3.obj")
 
 # TRAIN Agent
-DeepQN.train(int(1e7))
+DeepQN.train(int(1e7), testPer=10000)
 
 DeepQN.save("../../saved_models/CarRacing_v3/DQN_lr1e-3.obj")
