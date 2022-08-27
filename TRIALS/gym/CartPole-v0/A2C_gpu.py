0f257fcdd7e5544ba06dac45b7deb62518cdcff1
Jaehyun-Jeong <wogus0948@naver.com>
2022-08-27 08:06:45 +0000

    fixed A2C

---
+++ TRIALS/gym/CartPole-v0/A2C_gpu.py
@@ -0,0 +1,54 @@
+import sys
+sys.path.append("../../../") # to import module
+
+from datetime import datetime
+
+# 시작 시간
+startTime = datetime.now()
+
+# 파이토치
+import torch
+import torch.optim as optim
+
+# 작성자의 모듈
+from module.PolicyGradient.models import ANN_V3
+from module.PolicyGradient import A2C
+
+# 환경
+import gym
+env = gym.make('CartPole-v0')
+num_actions = env.action_space.n
+num_states = env.observation_space.shape[0]
+
+A2C_model = ANN_V3(num_states, num_actions)
+optimizer = optim.Adam(A2C_model.parameters(), lr=1e-4)
+
+# 작성자의 모듈 초기화
+advantage_AC = A2C(
+    env=env,
+    model=A2C_model,
+    device=torch.device('cuda'),
+    optimizer=optimizer,
+    verbose=0,
+    policy={
+        'train': 'stochastic',
+        'test': 'greedy',
+    },
+)
+
+# 모듈 초기화에 걸린 시간
+print(f"Init Time: {datetime.now() - startTime}")
+
+# 학습이 시작되는 시간
+startTrainTime = datetime.now()
+
+advantage_AC.train(
+        trainTimesteps=1000000,
+        testSize=0)
+
+# 학습이 끝나는 시간
+print(f"Train Time: {datetime.now() - startTrainTime}")
+
+# 성능 측정을 위한 테스트
+print(advantage_AC.test(testSize=10))
+print("=================================================")
---
+++ stable-baselines3/A2C_gpu.py
@@ -0,0 +1,49 @@
+from datetime import datetime
+
+# 시작 시간
+startTime = datetime.now()
+
+import gym
+from stable_baselines3 import A2C
+import torch
+
+env = gym.make("CartPole-v0")
+
+# 작성자의 모듈과 동일하게 파라미터 설정
+model = A2C(
+        "MlpPolicy",
+        env,
+        device=torch.device('cuda'),
+        gae_lambda=1,
+        use_rms_prop=False,  # RMSProp 옵티마이저 대신에 Adam을 사용
+        learning_rate=1e-4,
+        n_steps=10,
+        vf_coef=1,
+        verbose=0)
+
+# 모듈 초기화에 걸린 시간
+print(f"Init Time: {datetime.now() - startTime}")
+
+# 학습이 시작되는 시간
+startTrainTime = datetime.now()
+
+model.learn(
+        total_timesteps=1000000,  # 백만번의 학습을 시행
+        n_eval_episodes=0)
+
+# 학습이 끝나는 시간
+print(f"Train Time: {datetime.now() - startTrainTime}")
+
+# 성능 측정을 위한 테스트 코드
+returns = [0]*10
+for episode in range(10):
+    obs = env.reset()
+    while True:
+        action, state = model.predict(obs)
+        obs, reward, done, info = env.step(action)
+        returns[episode] += reward
+        if done:
+            break
+
+print(sum(returns) / 10)
+print("=================================================")
