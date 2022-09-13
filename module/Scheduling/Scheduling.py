4152f93ac842a5e31171811a5c3fada741b3ce4d
Jaehyun-Jeong <wogus0948@naver.com>
2022-09-13 01:27:20 +0000

    created Scheduling classes

---
+++ module/Scheduling/Scheduling.py
@@ -0,0 +1,63 @@
+import math
+
+
+class Scheduling():
+
+    def __init__(
+            self,
+            start: float,
+            end: float,
+            decay: int,):
+
+        self.start = start
+        self.end = end
+        self.decay = decay
+
+
+# Exponential Scheduling
+class expScheduling(Scheduling):
+
+    def __init__(
+            self,
+            start: float = 0.99,
+            end: float = 0.0001,
+            decay: int = 100000,):
+
+        super().__init__(
+                start=start,
+                end=end,
+                decay=decay,)
+
+    def get_eps(
+            self,
+            steps_done: int) -> float:
+
+        eps_threshold = \
+            self.end + (self.start - self.end) * \
+            math.exp(-1. * steps_done / self.decay)
+
+        return eps_threshold
+
+
+# Linear Scheduling
+class linScheduling(Scheduling):
+
+    def __init__(
+            self,
+            start: float = 0.99,
+            end: float = 0.0001,
+            decay: int = 100000,):
+
+        super().__init__(
+                start=start,
+                end=end,
+                decay=decay,)
+
+    def get_eps(
+            self,
+            steps_done: int) -> float:
+
+        eps_threshold = \
+            self.start - steps_done * (self.start - self.end) / self.decay
+
+        return eps_threshold
