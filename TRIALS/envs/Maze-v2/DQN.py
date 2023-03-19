c6acaa59c5f68c2573f9fd34e6f3dfa85cca1b8f
Jaehyun-Jeong <wogus0948@naver.com>
2023-03-19 17:01:23 +0000

    fixed MazeEnv

--- TRIALS/envs/Maze-v2/DQN.py
+++ TRIALS/envs/Maze-v2/DQN.py
@@ -10,13 +10,17 @@ from grll.VB import DQN
 
 # 환경
 from grll.envs.Maze import MazeEnv_v2
+
 trainEnv = MazeEnv_v2(exploring_starts=True)
 testEnv = MazeEnv_v2(exploring_starts=False)
 num_actions = trainEnv.num_action
 num_states = trainEnv.num_obs
 
+lr = float(sys.argv[1])
+discount = float(sys.argv[2])
+
 DQN_model = ANN_Maze(num_states, num_actions)
-optimizer = optim.Adam(DQN_model.parameters(), lr=1e-2)
+optimizer = optim.Adam(DQN_model.parameters(), lr=lr)
 
 # 작성자의 모듈 초기화
 DeepQN = DQN(
@@ -24,32 +28,31 @@ DeepQN = DQN(
     testEnv=testEnv,
     model=DQN_model,
     optimizer=optimizer,
-    verbose=1,
+    verbose=0,
     useTensorboard=True,
     maxTimesteps=int(1e100),
-    maxMemory=10000,
-    numBatch=16,
-    discount=0.95,
+    maxMemory=10000, # 8 times of state size
+    numBatch=12,
+    discount=discount,
     actionParams={
         # for DISCRETE
         'algorithm': "greedy",  # greedy, stochastic
         'exploring': 'epsilon',  # epsilon, None
         'exploringParams': {
-            'start': 0.1,
+            'start': 0.9,
             'end': 0.001,
-            'decay': 1e10,
+            'decay': 1000,
         },
     }, tensorboardParams={
-        'logdir': "../../runs/DQN_Maze_v0",
-        'tag': "Averaged Returns/ANN_Maze_lr=1e-4"
+        'logdir': "../../runs/DQN_Maze_v2",
+        'tag': f"Averaged Returns/ANN_Maze_lr={lr}_discount={discount}"
     },
-    epoch=1,
+    epoch=4,
+    gradientStepPer=4,
     trainStarts=1000,
 )
 
 DeepQN.train(
-        1e10,
-        testPer=1000,
+        10000,
+        testPer=10,
         testSize=1,)
-
-DeepQN.save("../../saved_models/MazeEnv_v2/DQN_Maze_v2.obj")
