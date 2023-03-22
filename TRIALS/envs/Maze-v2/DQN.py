fixed softmax system?

TRIALS/envs/Maze-v2/DQN.py
@@ -10,38 +10,51 @@ from grll.VB import DQN
 
 # 환경
 from grll.envs.Maze import MazeEnv_v2
+from grll.envs.Maze.utils import policy_diagram
 
 # 미로 정의
 import numpy as np
+from copy import copy
+'''
 maze = np.array([
-    [ 0.,  1.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.],
-    [ 0.,  0.,  0.,  0.,  0.,  1.,  0.,  0.,  0.,  0.],
-    [ 0.,  0.,  0.,  0.,  0.,  1.,  0.,  0.,  0.,  0.],
-    [ 1.,  1.,  0.,  1.,  1.,  0.,  1.,  0.,  0.,  0.],
-    [ 0.,  0.,  1.,  0.,  1.,  0.,  1.,  1.,  1.,  0.],
-    [ 0.,  0.,  1.,  0.,  1.,  0.,  0.,  0.,  0.,  0.],
-    [ 0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.],
-    [ 0.,  0.,  0.,  0.,  0.,  0.,  1.,  1.,  1.,  1.],
-    [ 0.,  1.,  1.,  1.,  1.,  1.,  0.,  0.,  0.,  0.],
-    [ 0.,  0.,  0.,  0.,  0.,  0.,  0.,  1.,  0.,  0.]
+    [0, 0, 1, 0, 0],
+    [1, 0, 1, 1, 0],
+    [1, 0, 1, 0, 0],
+    [1, 0, 0, 0, 1],
+    [1, 0, 1, 0, 0],
+])
+'''
+maze =  np.array([
+    [ 0.,  1.,  0.,  0.,  0.,  0.,  0.],
+    [ 0.,  0.,  0.,  1.,  1.,  0.,  1.],
+    [ 1.,  1.,  1.,  0.,  0.,  0.,  1.],
+    [ 0.,  0.,  0.,  0.,  1.,  1.,  0.],
+    [ 0.,  1.,  1.,  1.,  0.,  0.,  0.],
+    [ 0.,  1.,  0.,  0.,  0.,  0.,  0.],
+    [ 0.,  0.,  0.,  1.,  0.,  0.,  0.]
 ])
 
 trainEnv = MazeEnv_v2(
         exploringStarts=True,
-        mazeSize=(10, 10),
-        maze=maze
+        mazeSize=(7, 7),
+        maze=copy(maze)
         )
 testEnv = MazeEnv_v2(
         exploringStarts=False,
-        mazeSize=(10, 10),
-        maze=maze
+        mazeSize=(7, 7),
+        maze=copy(maze)
         )
+# testEnv.blocks = trainEnv.blocks
 
 num_actions = trainEnv.num_action
 num_states = trainEnv.num_obs
 
+'''
 lr = float(sys.argv[1])
 discount = float(sys.argv[2])
+'''
+lr = 0.002
+discount = 0.95
 
 DQN_model = ANN_Maze(num_states, num_actions)
 optimizer = optim.Adam(DQN_model.parameters(), lr=lr)
@@ -52,31 +65,33 @@ DeepQN = DQN(
     testEnv=testEnv,
     model=DQN_model,
     optimizer=optimizer,
-    verbose=0,
+    verbose=1,
     useTensorboard=True,
     maxTimesteps=int(1e100),
-    maxMemory=10000, # 8 times of state size
-    numBatch=12,
+    maxMemory=81*8, # 8 times of state size
+    numBatch=32,
     discount=discount,
     actionParams={
         # for DISCRETE
         'algorithm': "greedy",  # greedy, stochastic
         'exploring': 'epsilon',  # epsilon, None
         'exploringParams': {
-            'start': 0.9,
-            'end': 0.001,
-            'decay': 1000,
+            'start': 1,
+            'end': 0.1, 
+            'decay': 10000,
         },
     }, tensorboardParams={
         'logdir': "../../runs/DQN_Maze_v2",
         'tag': f"Averaged Returns/ANN_Maze_lr={lr}_discount={discount}"
     },
-    epoch=4,
-    gradientStepPer=4,
-    trainStarts=1000,
+    epoch=1,
+    gradientStepPer=1,
+    trainStarts=81*8,
 )
 
-DeepQN.train(
-        10000,
-        testPer=10,
-        testSize=1,)
+for _ in range(100):
+    DeepQN.train(
+            10000,
+            testPer=10000,
+            testSize=1,)
+    policy_diagram(testEnv, DeepQN)

grll/Policy/Policy.py
@@ -4,7 +4,7 @@ import random
 
 # PyTorch
 import torch
-import torch.nn as nn
+import torch.nn.functional as F
 from torch.distributions import Normal
 
 # module
@@ -89,9 +89,6 @@ class DiscretePolicy(Policy):
         # so we have to add some small number in log function
         self.ups = 1e-7
 
-        # Stochastic action selection
-        self.softmax = nn.Softmax(dim=0)
-
         # Initialize Parameters
         self.useEps = False
         self.useStochastic = False
@@ -111,7 +108,7 @@ class DiscretePolicy(Policy):
             isTest: bool = False,
             ) -> list:
 
-        probs = actionValue
+        probs = F.softmax(actionValue, dim=0)
 
         if isTest:
             eps = 0
@@ -120,8 +117,6 @@ class DiscretePolicy(Policy):
 
         if random.random() >= eps:
             if self.useStochastic:
-                probs = self.softmax(probs)
-
                 a = probs.multinomial(num_samples=1)
                 a = a.data
                 action = a[0].cpu()
@@ -158,8 +153,7 @@ class DiscretePolicy(Policy):
             S: torch.Tensor,
             A: torch.Tensor) -> torch.Tensor:
 
-        return torch.log(
-                self.softmax(self.pi(actionValue, S, A)) + self.ups)
+        return F.log_softmax(self.pi(actionValue, S, A))
 
 
 class ContinuousPolicy(Policy):